d696b5e6e5518f98412347c6466076dd93a81f6e
shohag2003 <60250894+shohag2003@users.noreply.github.com>
2020-01-24 14:14:24 +0600

    Sohag.py
    Hi

---
+++ Sohag.py
@@ -0,0 +1,23 @@
+print('''
+
+
+                  #welcome to facebook hacking tool#
+  _    _            _                      
+ | |  | |          | |                     
+ | |__| | __ _  ___| | 
+ |  __  |/ _` |/ __| |/ / 
+ | |  | | (_| | (__|          
+ |_|  |_|\__,_|\___|_|\_\
+    *  by-shohag khan*
+
+
+''')
+
+
+a=float(input('        inter fb id::'))
+
+b=input('        run {8080}:')
+	
+	
+print('show password: Dp2232')
+      
